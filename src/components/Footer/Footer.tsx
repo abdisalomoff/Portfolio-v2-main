917ff725c8215ed309dbdee3f9f64dd857c762dc
abdisalomoff <Avazbek6757@gmail.com>
2024-03-06 16:32:10 +0500

    first commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,48 @@
+import { Container } from './styles'
+import reactIcon from '../../assets/react-icon.svg'
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import telegram from '../../assets/telegram.svg'
+import instagramIcon from '../../assets/instagram.svg'
+
+export function Footer() {
+  return (
+    <Container className="footer">
+      <div>
+        <p>
+          This Website was made with <img src={reactIcon} alt="React" />
+        </p>
+      </div>
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/abdisalomoff"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/abdisalomoff/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+        <a
+          href="https://t.me/abdisalomoff"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={telegram} alt="telegram" />
+        </a>
+        <a
+          href="https://www.instagram.com/abdisalomoff"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagramIcon} alt="Instagram" />
+        </a>
+      </div>
+    </Container>
+  )
+}
